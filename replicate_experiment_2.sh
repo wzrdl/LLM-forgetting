275ae127123cd1c9f681ff405c8cb7d0a5359fec
ZR <wzrqczj@gmail.com>
2026-02-16 01:36:01 -0500

    table 2

--- replicate_experiment_2.sh
+++ replicate_experiment_2.sh
@@ -1,14 +1,14 @@
 echo "************************ replicating experiment 2 (rotated MNIST) ***********************"
 echo " >>>>>>>> Plastic (Naive) SGD "
 python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 1234
-python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --sedd 4567
-python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
+#python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --sedd 4567
+#python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
 
 echo ""
 echo " >>>>>>>> Stable SGD "
 python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.5 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 1234
-python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.5 --hiddens 256 --batch-size 10 --dropout 0.5 --sedd 4567
-python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.5 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
+#python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.5 --hiddens 256 --batch-size 10 --dropout 0.5 --sedd 4567
+#python -m stable_sgd.main --dataset rot-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.5 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
 
 echo ""
 echo ">>>>>>>>> Other Methods (ER, A-GEM, EWC)"
@@ -20,14 +20,14 @@ cd ../..
 echo "************************ replicating experiment 2 (permuted MNIST) ***********************"
 echo " >>>>>>>> Plastic (Naive) SGD "
 python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 1234
-python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 4567
-python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
+#python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 4567
+#python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
 
 echo ""
 echo " >>>>>>>> Stable SGD "
 python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 1234
-python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 4567
-python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
+#python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 4567
+#python -m stable_sgd.main --dataset perm-mnist --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
 
 echo ""
 echo ">>>>>>>>> Other Methods (ER, A-GEM, EWC)"
@@ -41,14 +41,14 @@ cd ../..
 echo "************************ replicating experiment 2 (Split CIFAR-100) ***********************"
 echo " >>>>>>>> Plastic (Naive) SGD "
 python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 1234
-python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 4567
-python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
+#python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 4567
+#python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.01 --gamma 1.0 --hiddens 256 --batch-size 10 --dropout 0.0 --seed 7891
 
 echo ""
 echo " >>>>>>>> Stable SGD "
 python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 1234
-python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 4567
-python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
+#python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 4567
+#python -m stable_sgd.main --dataset cifar100 --tasks 20 --epochs-per-task 1 --lr 0.1 --gamma 0.8 --hiddens 256 --batch-size 10 --dropout 0.5 --seed 7891
 
 echo ""
 echo ">>>>>>>>> Other Methods (ER, A-GEM, EWC)"
